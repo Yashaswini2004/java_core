d838b5315f2ac5a11a2f8f19d75d475200c60cd7
Yashaswini2004 <yashaswinic31@gmail.com>
2025-08-02 16:14:57 +0530

    this and super method

---
+++ Super.java
@@ -0,0 +1,22 @@
+class A{
+ public A(){
+    System.out.println("In A");
+ }
+ public A(int n){
+    System.out.println("In A int");
+ }
+}
+class B extends A{
+   public B(){
+    System.out.println("In B");
+   }
+   public B(int n){
+    this();
+    System.out.println("In B int");
+   }
+}
+public class Super{
+    public static void main(String[] args){
+      B obj=new B(5);
+    }
+}
