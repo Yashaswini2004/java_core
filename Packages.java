f5426e380148eb63694f972a513c1e7f67932155
Yashaswini2004 <yashaswinic31@gmail.com>
2025-08-03 20:07:56 +0530

    packages in java

---
+++ Packages.java
@@ -0,0 +1,7 @@
+import tools.BasicCalc;
+public class Packages{
+    public static void main(String[] args){
+        BasicCalc obj1=new BasicCalc();
+        System.out.println(obj1.add(3,4));
+    }
+}
